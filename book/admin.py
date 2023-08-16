50be5bd70f4388883bf321495824311de0129513
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-08-16 12:57:31 +0600

    show data on admin panner

--- book/admin.py
+++ book/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from book.models import BookStoreModel
 # Register your models here.
-
-admin.site.register(BookStoreModel)
+class BookStoreModelAdmin(admin.ModelAdmin):
+    list_display = ('id','author','book_name','edition','category','publication_date','last_published')
+admin.site.register(BookStoreModel,BookStoreModelAdmin)
